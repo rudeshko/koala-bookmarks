d1e9495813b1ef2d1c44999f67db5c8f5eebad02
Dima Rudeshko <dima@rudeshko.net>
2019-07-07 16:59:27 -0400

    Applied prettier

--- js/main.js
+++ js/main.js
@@ -1,61 +1,75 @@
-$(document).ready(function(){
-    //Get bookmarks
-    chrome.storage.sync.get("bookmarks", function(obj){
-        var bookmarks = obj.bookmarks;
-        
-        if(!bookmarks){
-            var emptyBookmark = {
-                name: "",
-                url: ""
-            };
+$(document).ready(function() {
+  //Get bookmarks
+  chrome.storage.sync.get('bookmarks', function(obj) {
+    var bookmarks = obj.bookmarks;
 
-            bookmarks = new Array();
-            for(var i = 0; i < 16; i++){
-                bookmarks.push(emptyBookmark);
-            }
+    if (!bookmarks) {
+      var emptyBookmark = {
+        name: '',
+        url: ''
+      };
+
+      bookmarks = new Array();
+      for (var i = 0; i < 16; i++) {
+        bookmarks.push(emptyBookmark);
+      }
+    }
+
+    var row,
+      i,
+      count = 0,
+      rowAdd;
+    for (row = 0; row < 4; row++) {
+      rowAdd = "<div class='row'>";
+      for (i = count; i < count + 4; i++) {
+        if (bookmarks[i]['url'] !== '' && bookmarks[i]['name'] !== '') {
+          var add = '';
+          add += "<div class='cell'>";
+          add +=
+            "<a class='dummy' href='" +
+            bookmarks[i]['url'] +
+            "' target='_blank'>";
+          add += "<div class='link'></div>";
+
+          add += "<div class='circle'>";
+          add += "<div class='icon'>";
+          add +=
+            "<img src='http://www.google.com/s2/favicons?domain=" +
+            bookmarks[i]['url'] +
+            "' />";
+          add += '</div>';
+          add += '</div>';
+
+          add += "<div class='title'>";
+          add += bookmarks[i]['name'];
+          add += '</div>';
+          add += '</a>';
+          add += '</div>';
+          rowAdd += add;
+        } else {
+          rowAdd +=
+            "<div class='cell'><a href='./options.html#" +
+            (i + 1) +
+            "' target='_blank'><div class='circle add'>+</div></a></div>";
         }
-        
-        var row, i, count = 0, part, rowAdd;
-        for(row = 0; row < 4; row++){
-            rowAdd = "<div class='row'>";
-            for(i = count; i < count + 4; i++){
-                if(bookmarks[i]['url'] !== "" && bookmarks[i]['name'] !== ""){
-                    var add = "";
-                    add += "<div class='cell'>";
-                    add += "<a class='dummy' href='" + bookmarks[i]['url'] + "' target='_blank'>";
-                        add += "<div class='link'></div>";
-                        
-                        add += "<div class='circle'>";
-                            add += "<div class='icon'>";
-                            add += "<img src='http://www.google.com/s2/favicons?domain=" + bookmarks[i]['url'] + "' />";
-                            add += "</div>";
-                        add += "</div>";
-                        
-                        add += "<div class='title'>";
-                        add += bookmarks[i]['name'];
-                        add += "</div>";
-                    add += "</a>";
-                    add += "</div>";
-                    rowAdd += add;
-                }else{
-                    rowAdd += "<div class='cell'><a href='./options.html#" + (i + 1) + "' target='_blank'><div class='circle add'>+</div></a></div>";
-                }
-            }
-            rowAdd += "</div>";
-            count += 4;
-            $("body").append(rowAdd);
-        }
-    });
+      }
+      rowAdd += '</div>';
+      count += 4;
+      $('body').append(rowAdd);
+    }
+  });
 });
 
-setTimeout(function(){
-    $(document).on("keyup", "body", function(e){
-        var key = parseInt(String.fromCharCode(e.keyCode));
-        if(key >= 1 && key <= 9){
-            var $tab = $("div.cell").eq(key - 1).children("a")[0];
-            if($tab !== undefined){
-                $tab.click();
-            }
-        }
-    });
+setTimeout(function() {
+  $(document).on('keyup', 'body', function(e) {
+    var key = parseInt(String.fromCharCode(e.keyCode));
+    if (key >= 1 && key <= 9) {
+      var $tab = $('div.cell')
+        .eq(key - 1)
+        .children('a')[0];
+      if ($tab !== undefined) {
+        $tab.click();
+      }
+    }
+  });
 }, 500);
--- js/options.js
+++ js/options.js
@@ -1,90 +1,103 @@
-function updateBookmarks(bookmarks){
-    //Display bookmarks
-    for(var i = 0; i < 16; i++){
-        if(bookmarks[i]["url"]){
-            $("div.selection-row[data-id=" + (i + 1) + "]").css("background-image", "url('http://www.google.com/s2/favicons?domain=" + bookmarks[i]["url"] + "')").attr("data-name", bookmarks[i]["name"]).attr("data-url", bookmarks[i]["url"]);
-        }else{
-            $("div.selection-row[data-id=" + (i + 1) + "]").css("background-image", "");
-        }
+function updateBookmarks(bookmarks) {
+  //Display bookmarks
+  for (var i = 0; i < 16; i++) {
+    if (bookmarks[i]['url']) {
+      $('div.selection-row[data-id=' + (i + 1) + ']')
+        .css(
+          'background-image',
+          "url('http://www.google.com/s2/favicons?domain=" +
+            bookmarks[i]['url'] +
+            "')"
+        )
+        .attr('data-name', bookmarks[i]['name'])
+        .attr('data-url', bookmarks[i]['url']);
+    } else {
+      $('div.selection-row[data-id=' + (i + 1) + ']').css(
+        'background-image',
+        ''
+      );
     }
+  }
 }
 
-$(document).ready(function(){
-    chrome.storage.sync.get("bookmarks", function(obj){
-        var bookmarks;
-        if(!obj.bookmarks){
-            var emptyBookmark = {
-                name: "",
-                url: ""
-            };
+$(document).ready(function() {
+  chrome.storage.sync.get('bookmarks', function(obj) {
+    var bookmarks;
+    if (!obj.bookmarks) {
+      var emptyBookmark = {
+        name: '',
+        url: ''
+      };
 
-            bookmarks = new Array();
-            for(var i = 0; i < 16; i++){
-                bookmarks.push(emptyBookmark);
-            }
-        }else{
-            bookmarks = obj.bookmarks;
-        }
+      bookmarks = new Array();
+      for (var i = 0; i < 16; i++) {
+        bookmarks.push(emptyBookmark);
+      }
+    } else {
+      bookmarks = obj.bookmarks;
+    }
 
-        updateBookmarks(bookmarks);
+    updateBookmarks(bookmarks);
 
-        $("div.selection-row").click(function(){
-            $("div.selection-row").removeClass("selected");
-            $(this).addClass("selected");
-            $("input[name=position]").val($(this).attr("data-id"));
+    $('div.selection-row').click(function() {
+      $('div.selection-row').removeClass('selected');
+      $(this).addClass('selected');
+      $('input[name=position]').val($(this).attr('data-id'));
 
-            $("input[name=name]").val($(this).attr("data-name")).focus();
-            $("input[name=url]").val($(this).attr("data-url"));
-        });
-
-        var addIndex = parseInt(window.location.hash.substr(1));
-        if(addIndex > 0 && addIndex <= 16){
-            $("div.selection-row[data-id=" + addIndex + "]").trigger("click");
-        }
-
-        $("form#createBookmark").submit(function(event){
-            if(!confirm("Add/Change the bookmark?")){
-                event.preventDefault();
-                return;
-            }
-
-            var name = $("input[name=name]").val();
-            var url = $("input[name=url]").val();
-            var position = $("input[name=position]").val() - 1;
-            
-            if(position < 0){
-                event.preventDefault();
-                return;
-            }
-            
-            if(!url){
-                var url = "";
-            }else{
-                if(!url.startsWith("http://") && !url.startsWith("https://")){
-                    url = "http://" + url;
-                }
-            }
-            
-            if(!name){
-                var name = url.toLowerCase().replace(/(http:\/\/|https:\/\/)+/g, "");
-            }
-            
-            var newBookmark = {
-                name: name,
-                url: url
-            };
-            bookmarks[position] = newBookmark;
-
-            //Using Chrome storage sync set method
-            chrome.storage.sync.set({"bookmarks": bookmarks}, function(){
-                $("input[type=text], input[type=hidden]").val("");
-                $("div.selection-row").removeClass("selected");
-                updateBookmarks(bookmarks);
-                window.location.hash = '';
-                alert("New bookmark has been set");
-            });
-
-            event.preventDefault();
-        });
+      $('input[name=name]')
+        .val($(this).attr('data-name'))
+        .focus();
+      $('input[name=url]').val($(this).attr('data-url'));
     });
+
+    var addIndex = parseInt(window.location.hash.substr(1));
+    if (addIndex > 0 && addIndex <= 16) {
+      $('div.selection-row[data-id=' + addIndex + ']').trigger('click');
+    }
+
+    $('form#createBookmark').submit(function(event) {
+      if (!confirm('Add/Change the bookmark?')) {
+        event.preventDefault();
+        return;
+      }
+
+      var name = $('input[name=name]').val();
+      var url = $('input[name=url]').val();
+      var position = $('input[name=position]').val() - 1;
+
+      if (position < 0) {
+        event.preventDefault();
+        return;
+      }
+
+      if (!url) {
+        var url = '';
+      } else {
+        if (!url.startsWith('http://') && !url.startsWith('https://')) {
+          url = 'http://' + url;
+        }
+      }
+
+      if (!name) {
+        var name = url.toLowerCase().replace(/(http:\/\/|https:\/\/)+/g, '');
+      }
+
+      var newBookmark = {
+        name: name,
+        url: url
+      };
+      bookmarks[position] = newBookmark;
+
+      //Using Chrome storage sync set method
+      chrome.storage.sync.set({ bookmarks: bookmarks }, function() {
+        $('input[type=text], input[type=hidden]').val('');
+        $('div.selection-row').removeClass('selected');
+        updateBookmarks(bookmarks);
+        window.location.hash = '';
+        alert('New bookmark has been set');
+      });
+
+      event.preventDefault();
+    });
+  });
 });
