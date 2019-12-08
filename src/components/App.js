f5d686cabfac78ee941da0cb72a153f985ee8c5e
Dima Rudeshko <dima@rudeshko.net>
2019-12-07 20:32:45 -0500

    Basic layout completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,65 @@
 import React, { useState, useEffect } from "react";
+import { getStoredBookmarks, updateStoredBookmarks } from "./chromeHelper";
 import "../sass/App.scss";
 
 const App = () => {
+  const layout = {
+    LAYOUT_4x4: {
+      x: 4,
+      y: 4
+    }
+  };
+
   /**
    * Define Hooks
    */
-  const [hook, setHook] = useState(0);
+  const [lastUpdated] = useState(new Date());
+  const [bookmarks, setBookmarks] = useState([]);
+  const [currentLayout] = useState(layout.LAYOUT_4x4);
 
   /**
    * On mount effect
    */
   useEffect(() => {
-    console.log("Ready...");
-  }, []);
+    const stored_bookmarks = getStoredBookmarks();
+
+    if (!stored_bookmarks || stored_bookmarks.length === 0) {
+      console.log("No bookmarks found, setting empty bookmarks...");
+      console.log("Layout:", currentLayout.x, "x", currentLayout.y);
+
+      const emptyBookmark = {
+        name: "",
+        url: ""
+      };
+
+      const list = [];
+      for (var i = 0; i < currentLayout.x * currentLayout.y; i++) {
+        list.push(emptyBookmark);
+      }
+
+      setBookmarks(list);
+    }
+  }, [lastUpdated, currentLayout.x, currentLayout.y]);
 
   /**
    * Output the component
    */
   return (
     <div className="container">
-      <div />
+      <div className="controls"></div>
+      <div className="bookmarks">
+        {bookmarks.map((value, index) => (
+          <div className="bookmark" key={index}>
+            {value.name && value.url ? (
+              <div className="tab"></div>
+            ) : (
+              <div className="blank">
+                <div className="add">+</div>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/components/chromeHelper.js
@@ -0,0 +1,9 @@
+export const getStoredBookmarks = () => {
+  return false;
+
+  //   chrome.storage.sync.get("bookmarks", function(obj) {
+  //     return obj.bookmarks;
+  //   });
+};
+
+export default {};
