31df776b250486e588c1ea0bad6cee9e45c39ed4
Dima Rudeshko <dima@rudeshko.net>
2020-01-14 08:51:19 -0500

    Added google analytics to the extension

--- src/helpers.js
+++ src/helpers.js
@@ -197,7 +197,7 @@ export const Layouts = {
 };
 
 export const DefaultSettings = {
-  version: "1.0.0",
+  version: "1.0.1",
   updatedFromVersion: null,
   joinedVersion: null,
   dragEnabled: true,
